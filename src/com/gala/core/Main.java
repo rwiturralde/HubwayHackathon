729c39e243451137a3b1ad7c1a4e6c6f4f0c2601
rwiturralde <iturralr@gmail.com>
2014-05-23 14:35:50 -0400

    MongoDB Hello World

--- src/com/gala/core/Main.java
+++ src/com/gala/core/Main.java
@@ -1,11 +1,42 @@
 package com.gala.core;
 
-public class Main {
+import java.net.UnknownHostException;
+import java.util.List;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+public class Main {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello world!");
 		System.out.println("Hello world again!");
+		
+		try {
+			MongoClient mongo = new MongoClient("localhost");
+			
+			DB db = mongo.getDB("testDatabase");
+			DBCollection coll = db.getCollection("testCollection");
+			
+			BasicDBObject doc = new BasicDBObject("name", "MongoDB")
+			    .append("type", "database")
+			    .append("count", 1)
+			    .append("info", new BasicDBObject("x", 203).append("y", 102));
+					coll.insert(doc);
+
+			DBObject myDoc = coll.findOne();
+			System.out.println(myDoc);
+					
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("Exiting main...");
 	}
 
 }
