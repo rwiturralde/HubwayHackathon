d351da554816c4a191ff554e31225604719e63fc
hstandring <hstandring88@gmail.com>
2014-05-23 15:38:56 -0400

    Adding logging dependency.

--- src/com/gala/core/Main.java
+++ src/com/gala/core/Main.java
@@ -1,11 +1,15 @@
 package com.gala.core;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+	static final Logger _logger = Logger.getLogger(Main.class);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-		System.out.println("Hello world again!");
+		_logger.info("Hello world!");
+		_logger.info("Hello world again!");
 	}
 
 }
