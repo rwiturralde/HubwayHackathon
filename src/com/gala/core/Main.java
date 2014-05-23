17307f997ae643229ee1d758c29bb25e78fe3dba
rwiturralde <iturralr@gmail.com>
2014-05-23 16:57:10 -0400

    - Merging with Heather's latest log4j changes - Working MongoDB Hello World

--- src/com/gala/core/Main.java
+++ src/com/gala/core/Main.java
@@ -1,12 +1,15 @@
 package com.gala.core;
 
 import java.net.UnknownHostException;
+import java.util.Date;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
 import org.apache.log4j.Logger;
 
 
@@ -20,14 +23,15 @@ public class Main {
 		_logger.info("Hello world again!");
 		
 		try {
-			MongoClient mongo = new MongoClient("localhost");
+			MongoClient mongo = new MongoClient(new MongoClientURI("mongodb://defaultUser:defaultUser@ds033459.mongolab.com:33459/hubwayhackathon"));
 			
-			DB db = mongo.getDB("testDatabase");
+			DB db = mongo.getDB("hubwayhackathon");
 			DBCollection coll = db.getCollection("testCollection");
 			
 			BasicDBObject doc = new BasicDBObject("name", "MongoDB")
 			    .append("type", "database")
 			    .append("count", 1)
+			    .append("time", new Date().toString())
 			    .append("info", new BasicDBObject("x", 203).append("y", 102));
 			coll.insert(doc);
 
@@ -35,7 +39,7 @@ public class Main {
 			_logger.info(myDoc);
 					
 		} catch (UnknownHostException e) {
-			_logger.error(e);
+			e.printStackTrace();
 		}
 		
 		_logger.info("Exiting main...");
