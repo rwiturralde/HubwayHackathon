edd85f9fb5ab95a8482eede43701096fbac8ccc2
rwiturralde <iturralr@gmail.com>
2014-05-28 11:40:20 -0400

    Cleaning up Main method to simply initialize the spring context and hook it to the JVM.  The rest of the app should be driven by Spring

--- src/com/gala/core/Main.java
+++ src/com/gala/core/Main.java
@@ -1,13 +1,5 @@
 package com.gala.core;
 
-import java.util.Date;
-
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-
 import org.apache.log4j.Logger;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,37 +9,16 @@ public class Main {
 	static final Logger _logger = Logger.getLogger(Main.class);
 	
 	public static void main(String[] args) {
-		
-		_logger.info("Hello world!");
-		
-		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
-		HubwayUser user = appContext.getBean("springTestUser", HubwayUser.class);
-		_logger.info("Test user info id: " + user.getUserId() + " age: " + user.getAge());
+		_logger.info("Launching application...");
 		
 		try {
-			MongoClient mongo = (MongoClient) appContext.getBean("mongoClient");
-			
-			DB db = mongo.getDB("hubwayhackathon");
-			
-			DBCollection coll = db.getCollection("testCollection");
-			
-			BasicDBObject doc = new BasicDBObject("name", "MongoDB")
-			    .append("type", "database")
-			    .append("count", 1)
-			    .append("time", new Date().toString())
-			    .append("info", new BasicDBObject("userId", user.getUserId()).append("age", user.getAge()));
-			coll.insert(doc);
-
-			DBObject myDoc = coll.findOne();
-			_logger.info(myDoc);
-					
+			ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("appContext.xml");
+			appContext.registerShutdownHook();
 		} catch (Exception e) {
 			_logger.error(e);
-		} finally {
-			appContext.close();
 		}
 		
-		_logger.info("Exiting main...");
+		_logger.info("Exiting Main...");
 	}
 
 }
