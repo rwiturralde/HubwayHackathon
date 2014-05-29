64bc067d780586eb5b87674b962f9ab3717fe535
rwiturralde <iturralr@gmail.com>
2014-05-29 13:42:53 -0400

    - Adding some dude's forecast.io java lib that wraps the forecast.io API and code to print the current weather in the console app - Fix to someone's spring derp

--- src/com/gala/ui/CommandLineUI.java
+++ src/com/gala/ui/CommandLineUI.java
@@ -1,5 +1,8 @@
 package com.gala.ui;
 
+import dme.forecastiolib.FIOCurrently;
+import dme.forecastiolib.ForecastIO;
+
 /**
  * Command-line based Hubway UI.  Allows the user to choose various parameters of their starting
  * location.  We then display the results of the logic engine based on that input. 
@@ -18,6 +21,23 @@ public class CommandLineUI implements IHubwayUI {
 	 */
 	public HubwayRequestParameters getUserParameters() {
 		HubwayRequestParameters userParams = null;
+		
+		ForecastIO fio = new ForecastIO("2e4135288e84ee81b4bb8418d67d6710");
+	    fio.setUnits(ForecastIO.UNITS_US);
+	    fio.getForecast("42.3963507", "-71.1226734");
+	    System.out.println("Latitude: "+fio.getLatitude());
+	    System.out.println("Longitude: "+fio.getLongitude());
+	    System.out.println("Timezone: "+fio.getTimezone());
+	    
+	    FIOCurrently currently = new FIOCurrently(fio);
+	    //Print currently data
+	    System.out.println("\nCurrently\n");
+	    String [] f  = currently.get().getFieldsArray();
+	    for(int i = 0; i<f.length;i++)
+	        System.out.println(f[i]+": "+currently.get().getByKey(f[i]));
+
+
+		
 		return userParams;
 	}
 
