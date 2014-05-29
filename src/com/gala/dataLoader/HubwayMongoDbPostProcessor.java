a2d983f07089903bf4ec079c6d0035601e7582eb
Belisle <belisle@galatea-associates.com>
2014-05-29 11:08:46 -0400

    Changes to Postprocessor and logging

--- src/com/gala/dataLoader/HubwayMongoDbPostProcessor.java
+++ src/com/gala/dataLoader/HubwayMongoDbPostProcessor.java
@@ -1,18 +1,31 @@
 package com.gala.dataLoader;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 public class HubwayMongoDbPostProcessor implements IDataPostProcessor <String, Object>{
 
+	static final Logger _logger = Logger.getLogger(HubwayMongoDbPostProcessor.class);
 	private String id; 
 	private String mongoId;
 	private DateFormat hubwayDateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm:ss", Locale.ENGLISH);
-	
-	public HubwayMongoDbPostProcessor(String id, DateFormat hubwayDateFormat){
+	private String dateIdToSpan;
+	private String dayOfWeekSpanName;
+	private String timeOfDaySpanName;
+		
+	public HubwayMongoDbPostProcessor(String id, String mongoId, DateFormat hubwayDateFormat, String dateIdToSpan, String dayOfWeekSpanName, String timeOfDaySpanName){
 		this.id = id;
+		this.mongoId = mongoId;
+		this.hubwayDateFormat = hubwayDateFormat; 
+		this.dateIdToSpan = dateIdToSpan;
+		this.dayOfWeekSpanName = dayOfWeekSpanName;
+		this.timeOfDaySpanName = timeOfDaySpanName;
 	}
 	
 	public Map<String, Object> postProcessDataEntry(Map<String, Object> map) {
@@ -25,15 +38,33 @@ public class HubwayMongoDbPostProcessor implements IDataPostProcessor <String, O
 			//log error
 		}
 		
+		Object dateObjToSpan;
+		if ((dateObjToSpan = map.get(dateIdToSpan)) != null){
+			Date dateToSpan = null;
+			try {
+				dateToSpan = hubwayDateFormat.parse(dateObjToSpan.toString());
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if (dateToSpan != null){
+				addDayOfWeekSpan(map, dateToSpan);
+				addTimeOfDaySpan(map, dateToSpan);
+			}
+		} else {
+			//log error
+		}
+		
 		return map;
 		
 	}
 	
-	private void addDayOfWeekBand(Map<String, Object> map){
+	private void addDayOfWeekSpan(Map<String, Object> map, Date dateToSpan){
 		
 	}
 	
-	private void addTimeOfDayBand(Map<String, Object> map){
+	private void addTimeOfDaySpan(Map<String, Object> map, Date dateToSpan){
 		
 	}
 
