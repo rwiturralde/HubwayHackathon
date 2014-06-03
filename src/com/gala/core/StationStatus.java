dba6c57996bf3e2f096f877c75e96337dff45fc0
Belisle <belisle@galatea-associates.com>
2014-06-03 11:05:07 -0400

    MongoDbDataRetriever fixes

--- src/com/gala/core/StationStatus.java
+++ src/com/gala/core/StationStatus.java
@@ -5,9 +5,32 @@ import java.util.Date;
 public class StationStatus {
 
 	protected int _numBikesAvailable;
+	protected int _capactity;
 	protected Date _date;
+	protected Day _day;
 	protected TimeOfDay _timeOfDay;
 	
+	public StationStatus(int numBikesAvailable_, int capactity_, Date date_,
+			Day day_, TimeOfDay timeOfDay_) {
+		super();
+		_numBikesAvailable = numBikesAvailable_;
+		_capactity = capactity_;
+		_date = date_;
+		_day = day_;
+		_timeOfDay = timeOfDay_;
+	}
+	public int getCapactity() {
+		return _capactity;
+	}
+	public void setCapactity(int capactity) {
+		_capactity = capactity;
+	}
+	public Day getDay() {
+		return _day;
+	}
+	public void setDay(Day day) {
+		_day = day;
+	}	
 	public int getNumBikesAvailable() {
 		return _numBikesAvailable;
 	}
